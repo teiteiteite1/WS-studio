c699ccc48c124af3285c96fc2e9ad11d42d4fe21
teiteiteite1 <yoshikisasaki889@gmail.com>
2026-09-04 09:32:22 +0900

    Add AI30 cloud sync controls

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import LearnClient from "./LearnClient";
+import LearnSyncBridge from "./LearnSyncBridge";
 import "./learn.css";
 
 export const metadata: Metadata = {
@@ -9,5 +10,10 @@ export const metadata: Metadata = {
 };
 
 export default function LearnPage() {
-  return <LearnClient />;
+  return (
+    <>
+      <LearnSyncBridge />
+      <LearnClient />
+    </>
+  );
 }
